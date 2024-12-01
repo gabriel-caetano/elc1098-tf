f6d8d65ccd2ac6901061aab6424d590324ab3e29
Gabriel Caetano <gabrielvscaetano@gmail.com>
2024-12-01 17:02:49 -0300

    delete unecessary files

--- tf.r
+++
@@ -1,7 +0,0 @@
-library(readxl)
-
-myDF <- data.frame(read_xlsx(path = "./ds/CAL_Ingressantes_e_formados_por_sexo_test.xlsx"))
-myDF2 <- data.frame(read_xls(path = "./ds/CCR Ingressantes e formados por sexo.xls"))
-
-head(myDF)
-head(myDF2)
